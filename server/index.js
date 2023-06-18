684b7e5119c0e9d0f3773d28189eccb6dd333912
MariAnn85 <marine_apresyan@mail.ru>
2023-06-19 02:49:07 +0500

    fourth commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 require('dotenv').config()
 const express=require('express')
 const sequelize = require('./db')
+const cors = require('cors')
+const models = require('./models/models')
+const { request } = require('express')
 const PORT=process.env.PORT || 3000
 
 const app=express()
+app.use(cors())
+app.use(express.json())
+
+
+
 
 const start = async () => {
     try {
